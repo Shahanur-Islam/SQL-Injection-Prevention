fb21d1842f5113fda6926cdbb5a3db9e0e3721f1
Shahanur Islam <shahanurpb@gmail.com>
2022-11-12 22:47:46 +0600

    Update database.php

--- database.php
+++ database.php
@@ -1,6 +1,9 @@
 <?php
 
-
+// email : shahanur@email.com
+// password: 1234' OR '1=1
+// Starting URL : http://localhost/dashboard/CSD/Shahanur/index.php?error=
+ 
 $host = "localhost";
 $dbname = "login_db";
 $username = "root";
